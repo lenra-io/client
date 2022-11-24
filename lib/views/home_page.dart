b35dd45b1ab72936b2b3cc98e8702e757919787c
jonas-martinez <cbobbxta.fr@gmail.com>
2022-11-24 11:51:58 +0100

    preparing feature before design

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,8 +1,30 @@
+import 'package:client_common/api/response_models/app_response.dart';
+import 'package:client_common/models/user_application_model.dart';
 import 'package:client_common/views/simple_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isInitialized = false;
+  List<AppResponse>? apps;
+
+  @override
+  void initState() {
+    context.read<UserApplicationModel>().getAppsUserOpened().then((value) {
+      setState(() {
+        apps = value;
+        isInitialized = true;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
