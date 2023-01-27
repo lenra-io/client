9b48ad23b447c3867f1660df4118e463d12f24a9
jonas-martinez <cbobbxta.fr@gmail.com>
2023-01-27 11:41:30 +0100

    ci: Fix dart doc generation

--- lib/navigation/url_strategy/url_strategy.dart
+++ lib/navigation/url_strategy/url_strategy.dart
@@ -1,2 +1,3 @@
+/// @nodoc
 export 'package:client_store/navigation/url_strategy/url_strategy_non_web.dart'
     if (dart.library.html) 'package:client_store/navigation/url_strategy/url_strategy_web.dart';
