9cb04900cc15ee76a28e6f0c8dfd66ff5935d8ed
Thomas DA ROCHA <8969556+taorepoara@users.noreply.github.com>
2023-01-30 12:07:39 +0100

    fix: Nginx conf to always display app (#54)

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_secure_storage_macos
 import package_info_plus_macos
 import sentry_flutter
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
 }
