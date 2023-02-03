bf644e1cafa6da00ce0f5c3a12426d1cbe97d4d1
jonas-martinez <cbobbxta.fr@gmail.com>
2023-02-03 10:58:14 +0100

    fix: Base URL nginx

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_macos
-import package_info_plus_macos
+import package_info_plus
 import sentry_flutter
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
