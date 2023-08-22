397c76ff75d4fa4b2072001e76120867215adc39
jonas.martinez <cbobbxta.fr@gmail.com>
2023-08-22 18:00:56 +0200

    feat: OAuth dynamic vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,7 @@ sed -i "s|\${LENRA_SERVER_DOMAIN}|${LENRA_SERVER_DOMAIN}|g" /opt/bitnami/nginx/c
 sed -i "s|\${LENRA_SERVER_URL}|${LENRA_SERVER_URL}|g" index.html
 sed -i "s|\${SENTRY_CLIENT_DSN}|${SENTRY_CLIENT_DSN}|g" index.html
 sed -i "s|\${OAUTH_CLIENT_ID}|${OAUTH_CLIENT_ID}|g" index.html
+sed -i "s|\${OAUTH_BASE_URL}|${OAUTH_BASE_URL}|g" index.html
+sed -i "s|\${OAUTH_REDIRECT_URL}|${OAUTH_REDIRECT_URL}|g" index.html
 
 /bin/bash -c "/opt/bitnami/scripts/nginx/entrypoint.sh $@"
