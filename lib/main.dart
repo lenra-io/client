b1aee4a23db69059c93ce2248f9f565c90180b24
shiipou <shiishii@nocturlab.fr>
2023-09-25 10:21:01 +0200

    fix: get environment from runtime
    Signed-off-by: shiipou <shiishii@nocturlab.fr>

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,10 @@ void main() async {
   });
 
   debugPrint("Starting main app[debugPrint]: ${Config.instance.application}");
-
-  const environment = String.fromEnvironment('ENVIRONMENT');
+  // ignore: todo
+  // TODO: Récupération de variables d'environnement ne doit pas marcher
+  const environment =
+      Config.instance.environment.length == 0 ? String.fromEnvironment('ENVIRONMENT') : Config.instance.environment;
 
   var reportMode = LenraReportMode();
   CatcherOptions debugOptions = CatcherOptions(
