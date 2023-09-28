a3240ade2a0d559641aeb09882369441e4e9aa4c
shiipou <shiishii@nocturlab.fr>
2023-09-28 09:14:04 +0200

    add environment in dockerfile
    Signed-off-by: shiipou <shiishii@nocturlab.fr>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 sed -i "s|\${LENRA_SERVER_DOMAIN}|${LENRA_SERVER_DOMAIN}|g" /opt/bitnami/nginx/conf/server_blocks/nginx.conf
 sed -i "s|\${LENRA_SERVER_URL}|${LENRA_SERVER_URL}|g" index.html
 sed -i "s|\${SENTRY_CLIENT_DSN}|${SENTRY_CLIENT_DSN}|g" index.html
+sed -i "s|\${SENTRY_CLIENT_DSN}|${ENVIRONMENT}|g" index.html
 sed -i "s|\${OAUTH_CLIENT_ID}|${OAUTH_CLIENT_ID}|g" index.html
 sed -i "s|\${OAUTH_BASE_URL}|${OAUTH_BASE_URL}|g" index.html
 sed -i "s|\${OAUTH_REDIRECT_URL}|${OAUTH_REDIRECT_URL}|g" index.html
